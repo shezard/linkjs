caac8deab809d54b5830b6fb376de1129cbce1d6
damien fayol <dam.fayol@gmail.com>
2011-12-26 20:59:42 +0100

    more examples

--- examples/app.js
+++ examples/app.js
@@ -21,19 +21,20 @@ window.onload = function() {
         if(next) setTimeout(function(){next()},1000);
       }
     }
-  })//.demo('first').sum(1,1,1).demo('last').reverse().sum(1,2,3).demo('1').demo('2').demo('3').sum(222,222,222).cb();
+  }).demo('first').sum(1,1,1).demo('last').reverse().sum(1,2,3).demo('1').demo('2').demo('3').sum(222,222,222).cb();
   
   // Example with Raphael.js
   
-  var paper = new Raphael(document.getElementById('canvas_container'), 400, 300);
+  var paper = new Raphael(document.getElementById('canvas_container'), 800, 800);
   
-  var circle = paper.circle(50,50,30).attr({'stroke':'none','fill':'rgba(255,0,0,.3)'});
+  var circle = paper.circle(50,50,30).attr({'stroke':'none','fill':'rgb(0,255,0)'});
+  
+  var rect = paper.rect(100,100,50,50).attr({'stroke':'none','fill':'rgb(0,255,0)'});
   
   linkjs({
     context : circle,
     funcs: {
       'animate' : function(attr,next) {
-        console.log(attr);
         if(next) {
           this.animate(attr,1e3,'easing',function(){next()});
         } else {
@@ -41,6 +42,19 @@ window.onload = function() {
         }
       }
     }
-  }).animate({'stroke-width':10,'stroke':'#000'}).animate({'stroke-width':5}).cb();
+  }).animate({'stroke-width':10,'stroke':'#000'}).animate({'stroke-width':5}).animate({'fill':'rgb(255,0,0)'}).cb();
+  
+  linkjs({
+    context : rect,
+    funcs : {
+      'rotate' : function(angle,next) {
+        if(next) {
+          this.animate({'transform':'r'+angle},1e3,'easing',function(){next()});
+        } else {
+          this.animate({'transform':'r'+angle},1e3,'easing');
+        }
+      }
+    }
+  }).rotate(-45).rotate(90).rotate(-360).cb();
   
 };
